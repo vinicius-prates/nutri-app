30af2c6f9c39b9e48b32fea74a71fa4f9106a1fc
vinicius-prates <viniprates2211@gmail.com>
2022-09-30 09:24:46 -0300

    fixed image 404 error

--- back-end/nutrition_app/nutrition_app/settings.py
+++ back-end/nutrition_app/nutrition_app/settings.py
@@ -1,5 +1,5 @@
 
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,6 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- back-end/nutrition_app/nutrition_app/urls.py
+++ back-end/nutrition_app/nutrition_app/urls.py
@@ -1,6 +1,9 @@
 from pictures.conf import get_settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +12,4 @@ urlpatterns = [
 
 urlpatterns += [
         path("_pictures/", include("pictures.urls")),
-    ]
+    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
