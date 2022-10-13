09b25f9d8a5c604d55aa1cccdef33bfe351b3cd4
vinicius-prates <viniprates2211@gmail.com>
2022-10-13 14:24:02 -0300

    added fodelete food feature

--- front-end/nutrition-app/src/pages/FoodPage.tsx
+++ front-end/nutrition-app/src/pages/FoodPage.tsx
@@ -1,11 +1,16 @@
 import { Footer } from "../components/Footer"
 import { NavBar } from "../components/NavBar"
-import { useParams } from "react-router-dom"
+import { Navigate, useParams } from "react-router-dom"
 import { Food } from "../components/LandingPage"
 import { useEffect, useState } from "react"
 import axios from "axios"
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { useNavigate } from "react-router-dom";
+
 
 export const FoodPage = () => {
+    const navigate =  useNavigate();
     
     const [Food, setFood] = useState<Food>()
     const {id} = useParams()
@@ -15,6 +20,28 @@ export const FoodPage = () => {
         axios.get(url).then(res => setFood(res.data))
         
     },[])
+
+
+    const confirmDelete = () => {
+        Confirm.show(
+            'Confirm Delete', 
+            'Do you want to delete this food?',
+            'Yes',
+            'No',
+            () => {
+                axios.delete(url)
+                Report.success('Food deleted!', '', 'OK!');
+                navigate('/')
+
+
+            },
+            () => {
+                Report.info('Food Delete canceled', '', 'OK!');
+                
+            })
+
+    }
+
     return(
 
         <div>
@@ -31,8 +58,9 @@ export const FoodPage = () => {
                     </div>
                     <div className="flex flex-col ">
                         <p className="mx-4 lg:text-xl lg:mb-10">{Food?.food_description}</p>
-                        <a className="text-right mx-4 text-lg italic  text-yellow-400 font-bold lg:mx-0" href={Food?.recipe_link}>Recipe link</a>
+                        <a className="text-right mx-4 text-lg italic   text-yellow-400 font-bold lg:mx-0" href={Food?.recipe_link}>Recipe link</a>
                         <p className="text-right mx-4 text-lg italic opacity-50 lg:mx-0">created at: {Food?.created_at}</p>
+                        <button className="text-center mx-4 text-xl opacity-50 hover:opacity-100 mt-4 cursor-pointer text-red-500 duration-150 " onClick={confirmDelete}>Delete food</button>
                     </div>
                     </div>
                     </div>
